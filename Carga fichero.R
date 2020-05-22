45e256785e099a90aa0ee0bec50d332a45a22e5e
Mikel <mikel.madariaga@alumni.mondragon.edugit status$ git status$ git add .git config --global user.email mikel.madariaga@alumni.mondragon.edu>
2020-05-22 09:57:31 +0200

    Limpiamos la columna de NIF

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,5 +1,8 @@
 load("E:/Descargas/archivos mikel/Universidad/Reto 4/DS/Examen_DS/Archivos_examen_ds/muestra26.RData")
 
+library(stringr)
+library(dplyr)
+
 dim(muestra26)
 colnames(muestra26)
 
@@ -8,3 +11,13 @@ colnames(muestra26)
 
 names(muestra26) = c("nif", "nombre", "genero")
 
+patron = "([[:digit:]]{8})([[:alpha:]]{1})"
+
+a=str_extract(muestra26$nif, patron)
+
+a=data.frame(a)
+
+muestra26=inner_join(a, muestra26, by = c("a" = "nif"))
+
+dim(muestra26)
+#Ahora el dataset tiene 306197 filas
