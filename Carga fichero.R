55b2d33e2efa1119a50ae770a574ff22b9a3b7cf
Mikel <mikel.madariaga@alumni.mondragon.edugit status$ git status$ git add .git config --global user.email mikel.madariaga@alumni.mondragon.edu>
2020-05-22 11:05:57 +0200

    Imputacion de genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,5 +1,8 @@
 load("E:/Descargas/archivos mikel/Universidad/Reto 4/DS/Examen_DS/Archivos_examen_ds/muestra26.RData")
 
+library(stringr)
+library(dplyr)
+
 dim(muestra26)
 colnames(muestra26)
 
@@ -8,3 +11,22 @@ colnames(muestra26)
 
 names(muestra26) = c("nif", "nombre", "genero")
 
+muestra26$nombre = iconv(muestra26$nombre, to="ASCII//TRANSLIT")#para eliminar problemas en el texto
+muestra26$nombre = str_to_title(muestra26$nombre) #con esto hacemos que los nombres esten en un formato normal, con los nombre y apellidos en mayusculas.
+muestra26$nombre = gsub("a\\?E", "n", muestra26$nombre ) #para corregir ñ
+muestra26$nombre = gsub("ñ", "n", muestra26$nombre )
+#muestra26$nombre = gsub('[[:punct:] ]+',' ',muestra26$nombre)
+
+#tablas de frecencias por genero
+hombres = filter(muestra26, muestra26$genero=="V")
+mujeres = filter(muestra26, muestra26$genero=="M")
+
+n_V = data.frame(table(hombres$nombre))
+n_M = data.frame(table(mujeres$nombre))
+
+#genero
+
+
+#imputacion de genero
+genero$genero_imp[genero$n_M>genero$n_V]="M"
+genero$genero_imp[genero$n_V>genero$n_M]="V"
