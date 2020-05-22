4f8cdda1c255b8532ec09f07204db755af7530b4
Mikel <mikel.madariaga@alumni.mondragon.edugit status$ git status$ git add .git config --global user.email mikel.madariaga@alumni.mondragon.edu>
2020-05-22 09:25:39 +0200

    Descripcion breve de Carga fichero.R

---
+++ Carga fichero.R
@@ -0,0 +1,7 @@
+load("E:/Descargas/archivos mikel/Universidad/Reto 4/DS/Examen_DS/Archivos_examen_ds/muestra26.RData")
+
+dim(muestra26)
+colnames(muestra26)
+
+#El dataset tiene 3 columnas y 334085 filas.
+#La primera columna es un codigo, la segunda el nombre y la tercera el sexo.
